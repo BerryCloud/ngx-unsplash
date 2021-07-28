e949afff43f133f561adc4b6811cdadb2dc15401
Thomas Turrell-Croft <thomasturrell@users.noreply.github.com>
2021-07-28 12:30:28 +0200

    Add search function
    - Create Angular library
    - Add documentation to readme

---
+++ projects/ngx-unsplash/src/lib/model/user.ts
@@ -0,0 +1,21 @@
+export interface User {
+  id: string;
+  username: string;
+  name: string;
+  first_name: string;
+  last_name: string;
+  instagram_username: string;
+  twitter_username: string;
+  portfolio_url: string;
+  profile_image: {
+    small: string;
+    medium: string;
+    large: string;
+  };
+  links: {
+    self: string;
+    html: string;
+    photos: string;
+    likes: string;
+  };
+}
