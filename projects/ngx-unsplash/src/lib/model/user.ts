6eae49f528a6e24157627d5c7a61091d992621df
Thomas Turrell-Croft <tom@berrycloud.co.uk>
2021-07-30 11:30:11 +0200

    wip

--- projects/ngx-unsplash/src/lib/model/user.ts
+++ projects/ngx-unsplash/src/lib/model/user.ts
@@ -1,21 +1,39 @@
 export interface User {
   id: string;
+  updated_at: string;
   username: string;
   name: string;
   first_name: string;
   last_name: string;
-  instagram_username: string;
   twitter_username: string;
   portfolio_url: string;
-  profile_image: {
-    small: string;
-    medium: string;
-    large: string;
-  };
+  bio: string;
+  location: string;
   links: {
     self: string;
     html: string;
     photos: string;
     likes: string;
   };
+  profile_image: ProfileImage;
+  instagram_username: string;
+  total_collections: number;
+  total_likes: number;
+  total_photos: number;
+  accepted_tos: boolean;
+  for_hire: boolean;
+  social: Social;
+}
+
+export interface ProfileImage {
+  small: string;
+  medium: string;
+  large: string;
+}
+
+export interface Social {
+  instagram_username: string;
+  portfolio_url: string;
+  twitter_username: string;
+  paypal_email?: any;
 }
