368c131396add4a1c834bad6efe540a305e06852
Thomas Turrell-Croft <tom@berrycloud.co.uk>
2021-09-29 14:41:07 +0200

    fix up

--- projects/ngx-unsplash/src/public-api.ts
+++ projects/ngx-unsplash/src/public-api.ts
@@ -5,5 +5,6 @@
 export * from './lib/model/photo';
 export * from './lib/model/search-result';
 export * from './lib/model/user';
+export * from './lib/pipes/blurhash.pipe';
 export * from './lib/unsplash.module';
 export * from './lib/unsplash.service';
