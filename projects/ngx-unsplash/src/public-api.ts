ad2762f9c082dd0f2a52b2417a641e8083adf6a5
Thomas Turrell-Croft <tom@berrycloud.co.uk>
2023-05-31 11:51:31 +0100

    sort alphabetically

--- projects/ngx-unsplash/src/public-api.ts
+++ projects/ngx-unsplash/src/public-api.ts
@@ -2,13 +2,13 @@
  * Public API Surface of ngx-unsplash
  */
 
-export * from './lib/model/photo';
-export * from './lib/model/search-result';
-export * from './lib/model/user';
 export * from './lib/model/collection';
 export * from './lib/model/download';
-export * from './lib/model/topic';
+export * from './lib/model/photo';
+export * from './lib/model/search-result';
 export * from './lib/model/statistics';
+export * from './lib/model/topic';
+export * from './lib/model/user';
 export * from './lib/pipes/blurhash.pipe';
 export * from './lib/unsplash.module';
 export * from './lib/unsplash.service';
